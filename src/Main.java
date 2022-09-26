4b9b16ab29a3203c32354ba0b6bfcce75df90743
dandy-step <dandy-step@users.noreply.github.com>
2022-09-26 17:26:16 +0100

    New comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world230!");
+        //comment
+	System.out.println("Hello world230!");
     }
 }
